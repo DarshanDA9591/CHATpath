9460fd8c7df9c18fb561183c5cba2846d629b6a1
Darshan DA <127665621+DarshanDA9591@users.noreply.github.com>
2024-05-14 15:40:34 +0530

    Update ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -5,9 +5,9 @@ console.log("id",process.env.VITE_CHAT_ENGINE_PROJECT_ID);
   return (
     <div className="background">
       <PrettyChatWindow
-        // projectId="d893e297-88c5-414d-a66c-88a64cbd9b21"
         
-        projectId={process.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        
+        projectId="YOUR_PROJECT_PROJECT_ID"
         username={props.user.username}
         secret={props.user.secret}
       />
